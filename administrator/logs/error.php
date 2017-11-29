0c1bb1fbd7cb8a7e16b14789b46b15b0f4dd5863
buluma <michael@buluma.me.ke>
2017-11-29 10:37:10 +0300

    profiles fix

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-11-29 05:22:14 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-11-29T05:22:13+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-11-29T05:22:38+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-11-29T05:23:07+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
